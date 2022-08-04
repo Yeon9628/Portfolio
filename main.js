c85f3d5fb4cf51cd4448abf80ce552f21cd8442a
Yeon9628 <ghdrl2648@gmail.com>
2022-08-04 18:46:47 +0900

    Final Touch!_2

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ navbarMenu.addEventListener('click', (event) => {
   }
   navbarMenu.classList.remove('open');
   scrollIntoView(link);
+  selectNavItem(target);
 });
 
 // Navbar toggle button for small screen
@@ -37,15 +38,15 @@ homeContactBtn.addEventListener('click', () => {
 
 // Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector('.home__container');
-const homeHeight = home.getBoundingClientRect().height;
+const homeHight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  home.style.opacity = 1 - window.scrollY / homeHeight;
+  home.style.opacity = 1 - window.scrollY / homeHight;
 });
 
 // Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', () => {
-  if (window.scrollY > homeHeight / 2) {
+  if (window.scrollY > homeHight / 2) {
     arrowUp.classList.add('visible');
   } else {
     arrowUp.classList.remove('visible');
@@ -73,6 +74,8 @@ workBtnContainer.addEventListener('click', (e) => {
   const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
   target.classList.add('selected');
 
+  
+
   projectContainer.classList.add('anim-out');
   setTimeout(() => {
     projects.forEach((project) => {
@@ -88,9 +91,69 @@ workBtnContainer.addEventListener('click', (e) => {
 });
 
 
+// 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다.
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+const sectionIds = [
+    '#home', 
+    '#about', 
+    '#skills',
+    '#work', 
+    // '#testimonials',
+    '#contact',
+];
 
+// 해당하는 모든 섹션 요소와 각각 메뉴 아이템 가져옴
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+console.log(sections);
+console.log(navItems);
 
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({behavior: "smooth"});
+// 선택된 메뉴 인덱스와 변수를 저장
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+// 새로운 메뉴 아이템을 선택할 때 마다
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active'); // 이전 선택 지워주고
+    selectedNavItem = selected; // 다시 새롭게 할당하고 나서
+    selectedNavItem.classList.add('active'); // 액티브를 지정해 준다.
 }
+
+// 나중에 또 쓰일 수 있기 때문에 함수로 하나 만들어놓고 호출만 할 수 있게.
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth'});
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
+}
+
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (Math.round(window.scrollY + window.innerHeight >= document.body.clientHeight)) {
+    selectedNavIndex = navItems.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
